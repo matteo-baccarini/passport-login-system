049f334d1ba5a4441a8d8af96019c3835a098fc1
matteo-baccarini <baccarinimatteo5@gmail.com>
2026-02-15 11:12:42 -0500

    full authentication system using passport-local

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
+if (process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
+
+const methodOverride = require('method-override')
 const express = require('express')
 const app = express()
 const bcrypt = require('bcrypt')
+const flash = require('express-flash')
+const session = require('express-session')
+
+const passport = require('passport')
+
+const initializePassport = require('./passport-config')
+initializePassport(
+    passport, 
+    email => { return users.find(users => users.email === email) },
+    id => {return users.find(users => users.id === id)}
+)
 
 //local variable for easy purposes
 const users = []
@@ -8,24 +24,37 @@ const users = []
 app.set('view-engine', 'ejs')
 //allows for access in the post method => req.body.name -> example
 app.use(express.urlencoded({extended : false}))
+app.use(flash())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
 
-app.get('/', (req, res) => { 
-    res.render('index.ejs', {name : 'Kyle'})
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
+
+app.get('/', checkAuthenticated,  (req, res) => { 
+    res.render('index.ejs', {name : req.user.name})
 })
 
-app.get('/login', (req, res) => {
+app.get('/login', checkNotAuthenticated, (req, res) => {
     res.render('login.ejs')
 })
 
-app.post('/login', (req, res) => {
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash : true
+}
+))
 
-})
-
-app.get('/register', (req, res) => {
+app.get('/register', checkNotAuthenticated, (req, res) => {
     res.render('register.ejs')
 })
 
-app.post('/register', async (req, res) => {
+app.post('/register', checkNotAuthenticated, async (req, res) => {
     try {
         //asynchronous function
         const hashedPassword = await bcrypt.hash(req.body.password, 10)
@@ -42,4 +71,26 @@ app.post('/register', async (req, res) => {
     console.log(users)
 })
 
+app.delete('/logout', (req, res) => {
+    req.logout(function(err) {
+        if (err) { return next(err) }
+        res.redirect('/login')
+    })
+})
+
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()){
+        return next()
+    }
+
+    res.redirect('/login')
+} 
+
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()){
+        return res.redirect('/')
+    }
+
+    next()
+}
 app.listen(3000)
